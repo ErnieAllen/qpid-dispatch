e7fb96017e8bceb50afcf7b40629c9a3de8d08fc
Ernest Allen <eallen@redhat.com>
2018-03-24 09:15:09 -0400

    Intermediate checkin

--- console/stand-alone/plugin/js/appTopo/qdrAppTopo.js
+++ console/stand-alone/plugin/js/appTopo/qdrAppTopo.js
@@ -17,7 +17,7 @@ specific language governing permissions and limitations
 under the License.
 */
 'use strict';
-/* global angular d3 */
+/* global angular d3 MicroService */
 
 var QDR = (function (QDR) {
   QDR.module.controller('QDR.AppTopoController', ['$scope', 'QDRService', '$location', '$timeout', function($scope, QDRService, $location, $timeout) {
@@ -59,14 +59,25 @@ var QDR = (function (QDR) {
     height = sizes[1];
     var svg = d3.select('#appTopo').append('svg')
       .attr('width', width)
-      .attr('height', height)
-      .append('g');
+      .attr('height', height);
 
     nodes = [];
+    var node = new MicroService();
+    node.info = {geo: {lat: 40, long: 40, city: 'Brno'}, ldap: 'ldap', nodeName: 'Canton'};
+    node.connections.push({});
+    node.connections.push({});
+    node.connections.push({});
+    nodes.push(node);
+    var node2 = new MicroService();
+    node2.connections.push({});
+    nodes.push(node2);
+    
+    /*
     nodes.push({
       type: d3.svg.symbolTypes[~~(Math.random() * d3.svg.symbolTypes.length)],
       size: Math.random() * 300 + 100
     });
+    */
     links = [];
   
     var force = d3.layout.force()
@@ -81,23 +92,70 @@ var QDR = (function (QDR) {
       //.on('end', function () {savePositions();})
       .start();
 
-    svg.selectAll("path")
-      .data(nodes)
-      .enter().append("path")
-      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
-      .attr("d", d3.svg.symbol()
-        .size(function(d) { return d.size; })
-        .type(function(d) { return d.type; }))
-      .style("fill", "steelblue")
-      .style("stroke", "white")
-      .style("stroke-width", "1.5px")
-      .call(force.drag);
-
-    function tick() {
-      svg.selectAll('path')
-        .attr('transform', function(d) { return 'translate(' + d.x + ',' + d.y + ')'; });
+    function dragstart(d) {
+      d3.select(this).classed('fixed', d.fixed = true);
     }
 
+    var drag = force.drag()
+      .on('dragstart', dragstart);
+  
+  
+    var serviceWidth = 100;
+    var serviceHeight = 200;
+    var diamondSize = 30;
+    var connectorR = 10;
+    var msg = svg.selectAll('.micro-service')
+      .data(nodes, function (d) { return d.id; })
+      .enter().append('svg:g')
+      .attr('class', 'micro-service');
+
+    msg.each(function (d) {
+      var cons = d.connections.length;
+      var h = Math.max(serviceHeight, cons*connectorR*2);
+      d3.select(this).append('svg:path')  // rectangle
+        .attr('class', 'service-container')
+        .attr('d', function () {
+          return d3.rect({x:0, y:0, width:serviceWidth, height: h});
+        });
+
+      var sp = (h - 2*connectorR*cons) / (cons + 1);
+      var cury = sp + connectorR;
+      d.connections.forEach( function (con, i) {
+        d3.select(this).append('svg:circle')  // connector
+          .attr('class', 'service-connection')
+          .attr('transform', 'translate(0,' + cury + ')')
+          .attr('r', connectorR);
+        cury += (sp + 2*connectorR);
+      }.bind(this));
+      var gDiamond = d3.select(this).append('svg:g')     // info diamond
+        .attr('transform', 'translate('+serviceWidth + ',' + (h - diamondSize*0.5)/2  + ')' );
+      gDiamond.append('svg:path')
+        .attr('class', 'service-info')
+        .attr('d', d3.svg.symbol().size(diamondSize*diamondSize/2).type('diamond'));
+      gDiamond.append('svg:text')
+        .attr('class', 'info-text')
+        .attr('y', 8*diamondSize/30)
+        .text( '?')
+        .attr('text-anchor', 'middle');
+    });
+    msg.call(drag);
+    /*
+    svg.selectAll('path')
+      .data(nodes, function(d) { return d.id; })
+      .enter().append('path')
+      .attr('transform', function(d) { return 'translate(' + d.x + ',' + d.y + ')'; })
+      .attr('d', d3.svg.symbol()
+        .size(function(d) { return d.size; })
+        .type(function(d) { return d.type; }))
+      .style('fill', 'steelblue')
+      .style('stroke', 'white')
+      .style('stroke-width', '1.5px')
+      .call(force.drag);
+*/
+    function tick() {
+      svg.selectAll('.micro-service')
+        .attr('transform', function(d) { return 'translate(' + d.x + ',' + d.y + ')'; });
+    }
 
 
   }]);
